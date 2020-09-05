3c612632dc728364db4dfb43d78ccc6b02c066e3
tanyudie <yudi@vodea.id>
2020-09-06 06:41:46 +0700

    temp commit

--- pages/live-detection.vue
+++ pages/live-detection.vue
@@ -8,31 +8,35 @@
       </b-col>
     </b-row>
 
-    <div style="background-color: red">
-      <video
-        ref="videoRef"
-        autoPlay="autoplay"
-        muted
-        playsInline
-        width="720"
-        height="600"
-      />
-      <canvas ref="canvasRef"
-              width="720"
-              height="650" />
+    <div class="row">
+      <div class="col-md-12">
+        <video
+          ref="videoRef"
+          autoPlay="autoplay"
+          muted
+          playsInline
+          width="720"
+          height="600"
+        />
+        <canvas ref="canvasRef"
+                width="720"
+                height="650" />
+      </div>
     </div>
 
-    <b-row class="mb-3">
+    <!--<b-row class="mb-3">
       <b-col md="12">
-        <video src="https://r3---sn-2uuxa3vh-jb36.googlevideo.com/videoplayback?expire=1599369685&ei=dB1UX4biOoPkgQf7nL6wDQ&ip=118.98.90.22&id=o-APKxqSPiRF_zb7sOMn_6wP9jG9DhRo42K2gOWC0BFxGs&itag=18&source=youtube&requiressl=yes&gcr=id&vprv=1&mime=video%2Fmp4&gir=yes&clen=19702711&ratebypass=yes&dur=232.478&lmt=1574747794247892&fvip=3&c=WEB&txp=5531432&sparams=expire%2Cei%2Cip%2Cid%2Citag%2Csource%2Crequiressl%2Cgcr%2Cvprv%2Cmime%2Cgir%2Cclen%2Cratebypass%2Cdur%2Clmt&sig=AOq0QJ8wRgIhALutSfO-I_2M9JUZPn190QOlFCyusw3RjKwffTN2OPZYAiEAoX4fQAHHj8XYLqoYVX0bhgGSMWOTZ2DbnQPFCMTP0dw%3D&video_id=DzwkcbTQ7ZE&title=Jessie+J+-+Flashlight+%28from+Pitch+Perfect+2%29+%28Official+Video%29&redirect_counter=1&rm=sn-nposr7s&req_id=3213d70fdb52a3ee&cms_redirect=yes&ipbypass=yes&mh=id&mip=36.71.57.145&mm=31&mn=sn-2uuxa3vh-jb36&ms=au&mt=1599348036&mv=m&mvi=3&pl=21&lsparams=ipbypass,mh,mip,mm,mn,ms,mv,mvi,pl&lsig=AG3C_xAwRQIgGmmBeLAIuNQ4AYj_QxmOR1NDV2NQswh_GTmuRR2doAMCIQDVpzOfY9EgLTF_nAtbp4lJd-ZgjwKdM8t7OjQBSjvEsQ%3D%3D"></video>
+        <video autoplay
+               src="https://r3-&#45;&#45;sn-2uuxa3vh-jb36.googlevideo.com/videoplayback?expire=1599369685&ei=dB1UX4biOoPkgQf7nL6wDQ&ip=118.98.90.22&id=o-APKxqSPiRF_zb7sOMn_6wP9jG9DhRo42K2gOWC0BFxGs&itag=18&source=youtube&requiressl=yes&gcr=id&vprv=1&mime=video%2Fmp4&gir=yes&clen=19702711&ratebypass=yes&dur=232.478&lmt=1574747794247892&fvip=3&c=WEB&txp=5531432&sparams=expire%2Cei%2Cip%2Cid%2Citag%2Csource%2Crequiressl%2Cgcr%2Cvprv%2Cmime%2Cgir%2Cclen%2Cratebypass%2Cdur%2Clmt&sig=AOq0QJ8wRgIhALutSfO-I_2M9JUZPn190QOlFCyusw3RjKwffTN2OPZYAiEAoX4fQAHHj8XYLqoYVX0bhgGSMWOTZ2DbnQPFCMTP0dw%3D&video_id=DzwkcbTQ7ZE&title=Jessie+J+-+Flashlight+%28from+Pitch+Perfect+2%29+%28Official+Video%29&redirect_counter=1&rm=sn-nposr7s&req_id=3213d70fdb52a3ee&cms_redirect=yes&ipbypass=yes&mh=id&mip=36.71.57.145&mm=31&mn=sn-2uuxa3vh-jb36&ms=au&mt=1599348036&mv=m&mvi=3&pl=21&lsparams=ipbypass,mh,mip,mm,mn,ms,mv,mvi,pl&lsig=AG3C_xAwRQIgGmmBeLAIuNQ4AYj_QxmOR1NDV2NQswh_GTmuRR2doAMCIQDVpzOfY9EgLTF_nAtbp4lJd-ZgjwKdM8t7OjQBSjvEsQ%3D%3D"></video>
       </b-col>
-    </b-row>
+    </b-row>-->
   </div>
 </template>
 
 <script>
 export default {
   mounted () {
+
     if (navigator.mediaDevices.getUserMedia || navigator.mediaDevices.webkitGetUserMedia) {
       // define a Promise that'll be used to load the webcam and read its frames
       const webcamPromise = navigator.mediaDevices
@@ -52,6 +56,7 @@ export default {
             }
           })
         }, (error) => {
+          alert(error)
           console.log("Couldn't start the webcam")
           console.error(error)
         })
@@ -66,6 +71,7 @@ export default {
           this.detectFromVideoFrame(values[0], this.$refs.videoRef.current)
         })
         .catch((error) => {
+          alert(error)
           console.error(error)
         })
     }
@@ -79,6 +85,7 @@ export default {
           this.detectFromVideoFrame(model, video)
         })
       }, (error) => {
+        alert(error)
         console.log("Couldn't start the webcam")
         console.error(error)
       })
